5d9211daf6e004780d7672783a623e2027b40191
carlgmail <carl.santoso@gmail.com>
2016-11-22 13:05:19 +0800

    custom.js
    Custom

---
+++ custom.js
@@ -0,0 +1,22 @@
+Hi
+This Workshop is available for 15 Trainee
+
+The attenddee are :
+1 . Andang Rian Dimas
+2 . Andika Priantama
+3 . Andiwijaya
+4 . Andreanus Agung Purnomo
+5 . Apriliana Nur Rahmawati
+6 . Ardi Ardianto
+7 . Iesa Luthfy Maliek
+8 . IGB Yoga Satria Perdana
+9 . Ilham Akbar Surya Permana
+10. Impola Tonatua Surya AO
+11. Indra Nugraha Putra
+12. Intan Puspitasari
+13. Irfan Bawa Suteja
+14. Irna Fitriani
+15. Bagus Wahyudi
+16. I Putu Yogi Pratama Putra
+
+Please joinning the session so we can try at least.
